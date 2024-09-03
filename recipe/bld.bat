4e3d2c611863caf9385f1612f1cf8a166261b16e
Mark Harfouche <mark.harfouche@gmail.com>
2024-09-03 12:35:52 -0400

    Speicify python executable for windows too

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,18 +1,14 @@
 mkdir build
 cd build
 
-set CONFIGURATION=Release
-
-cmake .. -G "%CMAKE_GENERATOR%" ^
-         -DCMAKE_BUILD_TYPE=%CONFIGURATION% ^
-         -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-         -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+cmake .. %CMAKE_ARGS% ^
+         -G "%CMAKE_GENERATOR%" ^
          -DBOOST_ROOT="%LIBRARY_PREFIX%" ^
          -DBUILD_NIFTY_PYTHON=ON ^
          -DWITH_HDF5=OFF ^
          -DWITH_Z5=ON ^
          -DWITH_ZLIB=ON ^
          -DWITH_BLOSC=ON ^
-         -DPYTHON_EXECUTABLE="%PYTHON%"
+         -DPython_EXECUTABLE="%PYTHON%"
 
-cmake --build . --config %CONFIGURATION% --target install
+cmake --build . --target install
